e7965dab556d0ec394f4fc88cb2d987893f2f1ec
Jose Garcia <joseg50328@students.laalliance.org>
2017-11-20 05:56:57 +0000

    done

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+createCanvas(800, 600)
+}
+
+function draw() {
+    noFill()
+    strokeWeight(4)
+    
+  ellipse(50, 50, 80, 80);
+  stroke("yellow")
+  ellipse(100, 100, 80, 80);
+  stroke("black")
+  ellipse(150, 50, 80, 80);
+  stroke("green")
+  ellipse(200, 100, 80, 80);
+  stroke("red")
+  ellipse(250, 50, 80, 80);
+  stroke("blue")
+}
